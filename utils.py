4a0d51f462a3b5801e3aca6a857aa8a04ae9d2de
Amirpasha Mobini Tehrani <105310430+amirpashamobinitehrani@users.noreply.github.com>
2023-03-16 16:32:31 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,37 +1,134 @@
-def normalized_to_pixel_coordinates(
-    normalized_x: float, normalized_y: float, image_width: int,
-    image_height: int) -> Union[None, Tuple[int, int]]:
-  """Converts normalized value pair to pixel coordinates."""
-
-  # Checks if the float value is between 0 and 1.
-  def is_valid_normalized_value(value: float) -> bool:
-    return (value > 0 or math.isclose(0, value)) and (value < 1 or
-                                                      math.isclose(1, value))
-
-  if not (is_valid_normalized_value(normalized_x) and
-          is_valid_normalized_value(normalized_y)):
-    # TODO: Draw coordinates even if it's outside of the image bounds.
-    return None
-  x_px = min(math.floor(normalized_x * image_width), image_width - 1)
-  y_px = min(math.floor(normalized_y * image_height), image_height - 1)
-  return x_px, y_px
+import torch
+import os
+from pythonosc import udp_client
+from pythonosc import osc_message_builder
+from pythonosc import dispatcher
+from pythonosc import osc_server
 
 
-def canny(image, low_thresh=100, high_thresh=200):
-  edges = cv2.Canny(image, low_thresh, high_thresh)
-  return edges
+def param_count(model):
+  return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 
-def is_blurry(image, thresh=150):
-  edges = cv2.Laplacian(image, cv2.CV_64F)
-  return edges.var() < thresh
+
+def find_max_epoch(path):
+    files = os.listdir(path)
+    epoch = -1
+    for f in files:
+        if len(f) <= 4:
+            continue
+        if f[-4:]  == '.pkl':
+            number = f[:-4]
+            try:
+                epoch = max(epoch, int(number))
+            except:
+                continue
+    return epoch
 
 
-def sharpen(image, kernel):
-  sharp = cv2.filter2D(resized_down, -1, kernel)
-  return sharp
+class UDP:
+
+    '''
+    Send UDP messages to Max msp
+    ''' 
+    
+    def __init__(self, message, oscSender):
+        self.oscSender = oscSender
+        self.message = message
+    
+    
+    def send(self):
+        msg = osc_message_builder.OscMessageBuilder(address = "/udp")
+        msg.add_arg(self.message)
+        self.oscSender.send(msg.build())
 
 
-def to_pixel_coords(relative_coords):
-    return tuple(round(coord * dimension) for coord, dimension in zip(relative_coords, SCREEN_DIMENSIONS))
+class Vowel:
 
+    '''
+    class that maps predicted tensors to target vowels
+    '''
+    def __init__(self,
+                 predictions):
+        
+        self.pred = predictions
+    
+    
+    def map(predictions):
+        vowel = None
+
+        if abs(predictions[0]) < 0.5:
+            vowel = 'c'
+        
+        elif abs(predictions[1]) < 0.8:
+            vowel = 'a'
+
+        elif abs(predictions[2]) < 0.9:
+            vowel = 'o'
+
+        elif abs(predictions[3]) < 0.9:
+            vowel = 'i'
+
+        elif abs(predictions[4]) < 0.7:
+            vowel = 'e'
+        
+        elif (abs(predictions[5])) < 1.8:
+            vowel = 'u'
+
+        return vowel    
+
+
+
+
+class Pitch:
+    
+    def __init__(self,
+                distance,
+                note_threshold,
+                pos_mean,
+                octave = False):
+                
+            self.distance = distance
+            self.note_threshold = note_threshold
+            self.pos_mean = pos_mean * 100
+            self.octave = octave
+            
+    
+    def pitch(self):
+    
+        count = 0
+        if self.distance < self.note_threshold and self.pos_mean  > 0 and self.pos_mean < 10 and count == 0:
+            notes = 62
+            count = 1
+        elif self.distance < self.note_threshold and self.pos_mean > 10 and self.pos_mean < 20 and count == 0:
+            notes = 60
+            count = 1
+        elif self.distance < self.note_threshold and self.pos_mean > 20 and self.pos_mean < 30 and count == 0:
+            notes = 59
+            count = 1
+        elif self.distance < self.note_threshold and self.pos_mean > 30 and self.pos_mean < 40 and count == 0:
+            notes = 57
+            count = 1
+        elif self.distance < self.note_threshold and self.pos_mean > 40 and self.pos_mean < 50 and count == 0:
+            notes = 55
+            count = 1
+        elif self.distance < self.note_threshold and self.pos_mean > 50 and self.pos_mean < 60 and count == 0:
+            notes = 53
+            count = 1
+        elif self.distance < self.note_threshold and self.pos_mean > 60 and self.pos_mean < 70 and count == 0:
+            notes = 52
+            count = 1
+        elif self.distance < self.note_threshold and self.pos_mean > 70 and self.pos_mean < 80 and count == 0:
+            notes = 50
+            count = 1
+        elif self.distance < self.note_threshold and self.pos_mean > 80 and self.pos_mean < 90 and count == 0:
+            notes = 48
+            count = 1
+        elif self.distance < self.note_threshold and self.pos_mean > 90 and self.pos_mean < 100 and count == 0:
+            notes = 47
+            count = 1
+        else:
+            count = 0
+            notes = None
+        
+        return notes
